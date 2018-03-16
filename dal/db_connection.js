04d88f2b145d743d8eb71a5deadef635677bff5e
AnalogHyperdrive <caribblacktip@yahoo.com>
2018-03-15 19:41:56 -0700

    CS355 Lab 9

---
+++ dal/db_connection.js
@@ -0,0 +1,6 @@
+exports.config = {
+    user: 'amiller',
+    password: '3347791',
+    host: 'blue.cs.sonoma.edu',
+    database: 'amiller_db',
+};
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CS355', subtitle:'Intro to Web Development' });
+});
+/* GET Template Example */
+router.get('/template_example', function(req, res, next) {
+  res.render('template_example', { first_name: req.query.first_name, last_name:req.query.last_name, title: 'CS355'})
+});
+/* GET resume view */
+router.get('/resume', function(req, res, next) {
+    res.render('resume_view', { first_name:req.query.first_name, last_name:req.query.last_name, major:req.query.major})
+});
+
+module.exports = router;
